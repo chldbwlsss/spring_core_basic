38b73e10ce357274a3c1dc17a0fde87db0891e81
youjin <developer_choi@naver.com>
2023-02-19 21:04:30 +0900

    start!

---
+++ src/test/java/hello/spring_core_basic/SpringCoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.spring_core_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
